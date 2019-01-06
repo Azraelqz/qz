6c8afcf8d59f9b0689e6a7acd8b6efab1ea044e0
qz <541396467@qq.com>
2019-01-06 21:00:59 +0800

    111

--- day01/Test01.java
+++ day01/Test01.java
@@ -8,7 +8,7 @@ public class Test01 {
 		System.out.println("假学如磨刀之石");
 		System.out.println("不见其损，年有所亏");
 		System.out.println("加油吧！少年");
-		System.out.println("J");
+		System.out.println("J");     
 		System.out.println("A");
 		System.out.println("V");
 		System.out.println("A");
